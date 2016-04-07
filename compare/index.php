92d4cc9d9b69a3bdd07ea2964e1789ee458d54f7
Jervy-EZ <jervy.zaballa@gmail.com>
2016-04-08 04:56:41 +0800

    update

--- compare/index.php
+++ compare/index.php
@@ -1,31 +1,143 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!DOCTYPE html>
+<html lang="en">
 <?php
-	require '../includes/connect.php';
-?>
-<head>
-    <title>Oro Site High School | Student Profiling</title>
-	<link href="img/logo1.gif" rel="icon">
-    <link href="extras/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/sb-admin-2.css" rel="stylesheet">    
-    <link href="extras/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-</head>
+include "includes/head.php";
 
+?>
 
 <body>
-<?php
-include "includes/navigation.php";?>
 
-   <a href="student.php" style="color:#FFFFFF; text-decoration:none">
-   <center><img src="img/2.png"></a>
-  
-   <a href="organization.php" style="color:#FFFFFF; text-decoration:none">
-   <img src="img/3.png"></a>
- 
-   <a href="guidance.php" style="color:#FFFFFF; text-decoration:none">
-   <img src="img/4.png"></a></center>
-                            </div></div></div>
+
+<?php
+include "includes/navigation.php";
+?>
+        
+
+                <div class="col-lg-12 col-md-12" style="padding-top: 3%;">
+              
+                       
+                    <div class="row">
+                    	
+                        <div class="col-md-12" style="padding-top:1%;">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-1"></div>    
+							<?php
+								$array=sql();
+								if($array['stud_info']==1){
+							?>
+                            <div class="col-md-2"><a href="student_selection.php" style="color:#FFFFFF; text-decoration:none;">
+                                <button class="btn  btn-primary  bg-blue btn-block btn-flat" style="padding:30px; background-color:#191970;">
+                                    <i class="fa fa-group fa-5x"></i> <br>                                     
+                                    <strong style="margin-left:-15px;">Student Information</strong>
+                                </button></a>
+                            </div> 
+                            <?php
+								}
+								else{
+							?>
+                            
+                            
+                            <?php }?>
+                            
+                            <?php
+								if($array['stud_info']==1){
+							?>
+                           <div class="col-md-2"><a href="../e-reg/reportStudType.php" style="color:#FFFFFF; text-decoration:none">
+                                <button class="btn btn-primary bg-blue btn-block btn-flat" style="padding:30px; background-color:#191970;" name="document">
+                                    <i class="fa fa-bar-chart-o fa-5x"></i> <br> 
+                                    <strong style="margin-left:-15px;">Statistics</strong>
+                                </button></a>
+                            </div>
+                             <?php
+								}
+								else{
+							?>
+                            
+                            <?php }?>
+                            <div class="col-md-2"></div>
+                            <br><br><br><br><br><br><br><br><br>
+                            <div class="col-md-3"></div>
+                            <?php
+								
+								if($array['act']==1){
+							?>
+                            <div class="col-md-2"><a href="activityDB.php" style="color:#FFFFFF; text-decoration:none">
+                                <button class="btn btn-primary bg-blue btn-block btn-flat" style="padding:30px; background-color:#191970;" name="activity">
+                                    <i class="fa fa-tasks fa-5x"></i> <br> 
+                                    <strong style="margin-left:-15px;">Activities</strong>
+                                </button></a>
+                            </div>
+                             <?php
+								}
+								else{
+							?>
+       						
+                            <?php
+								}
+								if($array['org']==1){
+							?>
+                            <div class="col-md-2"><a href="organizationDB.php" style="color:#FFFFFF; text-decoration:none">
+                                <button class="btn btn-primary bg-blue btn-block btn-flat" style="padding:30px; background-color:#191970;" name="activity">
+                                    <i class="fa fa-sitemap fa-5x"></i> <br> 
+                                    <strong style="margin-left:-15px;">Organizations</strong>
+                                </button></a>
+                            </div>
+                             <?php
+								}
+								else{
+							?>
+                           
+                            <?php
+								}
+								if($array['violtn']==1){
+							?>
+                            <div class="col-md-2"><a href="violationDB.php" style="color:#FFFFFF; text-decoration:none">
+                                <button class="btn btn-primary bg-blue btn-block btn-flat" style="padding:30px; background-color:#191970;" name="activity">
+                                    <i class="fa fa-legal fa-5x"></i> <br> 
+                                    <strong style="margin-left:-15px;">Violations & Sanctions</strong>
+                                </button></a>
+                            </div>
+                            <?php
+								}
+								else{
+							?>
+                            
+                            <?php
+								}
+								
+							?>
+                        	
+                            
+                            
+                        </div>
+
+                    </div>
+                     
+                          
+                 
+                    <!-- /.panel .chat-panel -->
+                </div>
+					
+
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="bower_components/raphael/raphael-min.js"></script>
+    <script src="bower_components/morrisjs/morris.min.js"></script>
+    <script src="js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="dist/js/sb-admin-2.js"></script>
 
 </body>
+
 </html>
--- compare/student_section.php
+++ compare/student_section.php
@@ -1,102 +1,139 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php
-	require '../includes/connect.php';
+<!DOCTYPE html>
+<html lang="en">
 
+<?php include "includes/head.php";
+include "storescripts/connect_to_mysql.php";
 ?>
 <?php
-include "includes/navigation.php";?>
-<head>
-    <title>Oro Site High School | Student Profiling</title>
-	<link href="img/logo1.gif" rel="icon">
-    <link href="extras/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/sb-admin-2.css" rel="stylesheet">    
-    <link href="extras/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-</head>
-
-
-<body> 
-<?php
-		if(isset($_GET['id'])){
-			$select_id=preg_replace('#[^0-9]#i','',$_GET['id']);
-			$sql=mysql_query("SELECT * FROM er_section WHERE er_sec_id='$select_id' LIMIT 1");
-			$count=mysql_num_rows($sql);
-			if($count > 0) {
-				while($row=mysql_fetch_array($sql)){
-					$section_id = $row["er_sec_id"];
-					$er_section_name = $row["er_sec_name"];
-					$er_grade_and_section = $row["er_sec_grade_level"];
-		}}else {
+if(isset($_GET['viewid'])){
+	$targetID=preg_replace('#[^0-9]#i','',$_GET['viewid']);
+	$sqlO=mysql_query("SELECT * FROM er_section WHERE er_sec_id='$targetID' LIMIT 1");
+	$actCount=mysql_num_rows($sqlO);
+	if($actCount > 0) {
+		while($row=mysql_fetch_array($sqlO)){
+			$sec_id = $row["er_sec_id"];
+			$er_sec_name = $row["er_sec_name"];
+			$er_sec_grade_level = $row["er_sec_grade_level"];
+		}
+	}else {
 		echo "No record found!";
 		exit();	
-		}}else{
-		echo "No record found";
-	exit();}?>
+	}
+}else{
+	echo "No record found";
+	exit();
+}
+?>
 <?php
-	$arr=sql();
-	$list2 = "";
+$array=sql();
+$dynamicList2 = "";
 	
-		$q2 = mysql_query("SELECT * FROM sp_student");
-		$count2 = mysql_num_rows($q2);
-		if($count2 > 0) {
-			while($row = mysql_fetch_assoc($q2)){
-	  $Student_ID = $row["sp_stud_id"];
-					$row["sp_stud_LRN"];
-					$row["sp_stud_Lname"];
-					$row["sp_stud_Fname"];
-					$row["sp_stud_Minitial"];
-					$row["sp_stud_house_no"];
-					$row["sp_stud_brgy"];
-					$row["sp_stud_city"];
-					$row["sp_stud_province"];
-					$row["sp_stud_age"];
-	  $Student_Birthday =strftime("%B %d %Y", strtotime($row["sp_stud_birthdate"]));
-					$row["sp_stud_gender"];
-					$row["sp_stud_contact_no"];
-					$row["sp_stud_religion"];
-					$row["sp_stud_mother_tongue"];
-					$row["sp_stud_IP"];
-					$row["sp_stud_prev_school_attended"];
-					$row["sp_stud_type"];
-					$row["sp_remarks"];
-		
-		$list2 .= '<tr>
-<td>' . $row["sp_stud_id"]. '</td><td>' . $row["sp_stud_LRN"] .',&nbsp;'.row["sp_stud_Fname"].'&nbsp;'.$row["sp_stud_Minitial"].'</td><td>' . $row["sp_stud_gender"] . '</td><td>' . $row["sp_stud_birthdate"]. '</td><td>' . $row["sp_stud_age"] . '</td><td>' . $row["sp_stud_mother_tongue"]. '</td><td>' . $row["sp_stud_IP"] . '</td><td>' . $row["sp_stud_religion"] . '</td><td>' . $row["sp_stud_house_no"] . '</td><td>' . $row["sp_stud_brgy"] . '</td><td>' . $row["sp_stud_city"] . '</td><td>' . $row["sp_stud_province"]. '</td>
+$query2 = mysql_query("SELECT * FROM sis_student WHERE er_sec_id='$targetID'");
+$employeeCount2 = mysql_num_rows($query2);
+if($employeeCount2 > 0) {
+	while($row = mysql_fetch_array($query2)){
+			$Student_ID = $row["SIS_stud_id"];
+		$Student_LRN = $row["SIS_stud_LRN"];
+		$stud_Lname = $row["SIS_stud_Lname"];
+		$stud_Fname = $row["SIS_stud_Fname"];
+		$stud_Mname = $row["SIS_stud_Minitial"];
+		$Student_House_No = $row["SIS_stud_house_no"];
+		$Student_Barangay = $row["SIS_stud_brgy"];
+		$Student_City = $row["SIS_stud_city"];
+		$Student_Province = $row["SIS_stud_province"];
+		$Student_Age = $row["SIS_stud_age"];
+		$Student_Birthday = $row["SIS_stud_birthdate"];
+		$Student_Sex = $row["SIS_stud_gender"];
+		$Student_Contact_No = $row["SIS_stud_contact_no"];
+		$Student_Religion = $row["SIS_stud_religion"];
+		$Student_Mother_Tongue = $row["SIS_stud_mother_tongue"];
+		$Student_IP = $row["SIS_stud_IP"];
+		$Student_Prev_School_Attended = $row["SIS_stud_prev_school_attended"];
+		$Student_Type = $row["SIS_stud_type"];
+		$Student_Remarks = $row["SIS_remarks"];
+		$dynamicList2 .= '<tr>
+						<td>' . $Student_LRN . '</td>
+						<td>' . $stud_Lname .',&nbsp;'.$stud_Fname.'&nbsp;'.$stud_Mname.'</td>
+						<td>' . $Student_Sex . '</td>
+						<td>' . $Student_Birthday . '</td>
+						<td>' . $Student_Age . '</td>
+						<td>' . $Student_Mother_Tongue . '</td>
+						<td>' . $Student_IP . '</td>
+						<td>' . $Student_Religion . '</td>
+						<td>' . $Student_House_No . '</td>
+						<td>' . $Student_Barangay . '</td>
+						<td>' . $Student_City . '</td>
+						<td>' . $Student_Province . '</td>
 						<td>' . 
-		<a href='student_profile_new.php?viewid=$Student_ID' class='fa fa-user fa-5x' title='View Profile'></a> . '</td>
-						</tr>';
-	}}
-	?>
-	
-          <div class="row">
+								"
+								<a href='student_profile_new.php?viewid=$Student_ID' class='fa fa-user'>&nbsp;View Profile</a>
+								" 
+						. '</td>
+						 </tr>';
+	}
+}else {
+	$dynamicList = "No record yet";	
+}
+mysql_close();
+?>
+<body>
+<?php
+include "includes/navigation.php";
+?>
+    <div id="wrapper">
+
+
+
+<div class="col-lg-12 col-md-12" style="padding-bottom:7%; padding-top: 2%;">
+<!--============================================================================================================================================================================ -->
+            <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading"> 
-                            <a href="student.php" align="right"><img src="img/GoBackButton.png" height="50": width="50"></a>    
- <?php						 	
-	if($array['sched']==1){?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	
-						<a href="schedule.php?getid=<?php echo $section_id ?>" align="right">View Schedule</a>
-							<?php }?> 
-                            <?php if($array['sched']==1){?>
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                            <a href="subjects.php?getid=<?php echo $section_id ?>" align="right">View Subjects</a>
-                            <?php }?>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-       <h3><center>List of Students in Grade <em><strong><?php echo $er_sec_grade_level ?></strong></em> Section <em><strong>
-	   <?php echo $er_sec_name ?>
-	   | S.Y. 2016-2017</strong></em></center></h3>
-           <p><center>Adviser:<strong><?php echo $er_section_name ?></strong></center></p>
+                        <div class="panel-heading">
+							List of Students in Grade <em><strong><?php echo $er_sec_grade_level ?></strong></em> Section <em><strong><?php echo $er_sec_name ?></strong></em>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            
+                            <a href="student.php" align="right">Go Back</a>
+                            
+                            
+                             <?php
+							 	
+								if($array['sched']==1){
+							?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<a href="schedule.php?getid=<?php echo $sec_id ?>" align="right">View Schedule</a>
+							<?php
+								}
+							?>
+                            
+                            
+                            <?php
+								if($array['sched']==1){
+							?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            <a href="subjects.php?getid=<?php echo $sec_id ?>" align="right">View Subjects</a>
+                            <?php
+								}
+							?>
                         </div>
+                        <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="dataTable_wrapper">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example" style="font-size:12px">
                                     <thead>
                                         <tr>
-                                        	 <th>LRN</th>	<th>Name</th>	<th>Sex</th>	<th>Birthdate</th>	<th>Age</th>
-                                             <th>Mother Tongue</th>		<th>IP</th>		<th>Religion</th>		<th>House No.</th>
-                                             <th>Barangay</th>	<th>City</th>	<th>Province</th>	<th>Actions</th>
+                                        	
+                                            <th>LRN</th>
+                                            <th>Name</th>
+                                            <th>Sex</th>
+                                            <th>Birthdate</th>
+                                            <th>Age</th>
+                                            <th>Mother Tongue</th>
+                                            <th>IP</th>
+                                            <th>Religion</th>
+                                            <th>House No.</th>
+                                            <th>Barangay</th>
+                                            <th>City</th>
+                                            <th>Province</th>
+                                            <th>Actions</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -104,7 +141,30 @@ include "includes/navigation.php";?>
                                     
                                     </tbody>
                                 </table>
-                          </div></div></div></div></div></div></div>
+                          </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        </div>
+                    </div>
+                </div>
+           
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include("includes/script_foot.php"); ?>
+
+
 </body>
 
 </html>
